22dd4b0c892960937600cec1210e7c484c2e8be7
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-02-28 19:19:22 +0100

    Add page utilites
    Supports #168

--- components/nspanel_ha_blueprint/pages.h
+++ components/nspanel_ha_blueprint/pages.h
@@ -21,7 +21,7 @@ namespace nspanel_ha_blueprint {
     * These names correspond to various pages of the Nextion TFT file in use,
     * such as settings, home, weather information, and more.
     */
-    constexpr std::array<const char*, 27> page_names = {
+    constexpr std::array<const char*, 28> page_names = {
         "home",
         "weather01",
         "weather02",
@@ -48,7 +48,8 @@ namespace nspanel_ha_blueprint {
         "alarm",
         "keyb_num",
         "media_player",
-        "confirm"
+        "confirm",
+        "utilities"
     };
 
     /**
