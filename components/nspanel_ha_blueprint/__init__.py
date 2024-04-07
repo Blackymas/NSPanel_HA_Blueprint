d368cb2239ed22ce33d3617040e2fe53b6da1d99
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-04-07 20:00:06 +0200

    Add config param to build in ESP-IDF
    Adds to #2024

--- components/nspanel_ha_blueprint/__init__.py
+++ components/nspanel_ha_blueprint/__init__.py
@@ -20,12 +20,20 @@ async def to_code(config):
         cg.add_build_flag("-DCONFIG_D0WD_PSRAM_CS_IO=18")
 
     if CORE.using_esp_idf:
+        add_idf_sdkconfig_option("CONFIG_D0WD_PSRAM_CLK_IO", 5)
+        add_idf_sdkconfig_option("CONFIG_D0WD_PSRAM_CS_IO", 18)
         add_idf_sdkconfig_option("CONFIG_BT_ALLOCATION_FROM_SPIRAM_FIRST", True)
         add_idf_sdkconfig_option("CONFIG_BT_BLE_DYNAMIC_ENV_MEMORY", True)
         add_idf_sdkconfig_option("CONFIG_ESP32_REV_MIN_3", True)
-        add_idf_sdkconfig_option("CONFIG_D0WD_PSRAM_CLK_IO", 5)
-        add_idf_sdkconfig_option("CONFIG_D0WD_PSRAM_CS_IO", 18)
+        add_idf_sdkconfig_option("CONFIG_LWIP_MAX_SOCKETS", 5)  # Is this adding any advantage here?
+        add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_BUFFER", True)
+        add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_FREE_CA_CERT", True)
+        add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_FREE_CONFIG_DATA", True)
+        add_idf_sdkconfig_option("CONFIG_MBEDTLS_EXTERNAL_MEM_ALLOC", True)
+        add_idf_sdkconfig_option("CONFIG_SPIRAM_ALLOW_BSS_SEG_EXTERNAL_MEMORY", True)  # This makes a significant improvement on RAM
+        add_idf_sdkconfig_option("CONFIG_SPIRAM_IGNORE_NOTFOUND", True)
+        add_idf_sdkconfig_option("CONFIG_SPIRAM_RODATA", True)
+        add_idf_sdkconfig_option("CONFIG_SPIRAM_TRY_ALLOCATE_WIFI_LWIP", True)
 
     cg.add_define("USE_NSPANEL_HA_BLUEPRINT")
     cg.add_global(nspanel_ha_blueprint_ns.using)
-
