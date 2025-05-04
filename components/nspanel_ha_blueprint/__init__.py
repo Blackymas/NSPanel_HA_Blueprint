ed34d177a2234a7da236929b05927439d896d5d6
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-05-04 06:29:35 +0200

    Clean-up build flags
    Remove redundances

--- components/nspanel_ha_blueprint/__init__.py
+++ components/nspanel_ha_blueprint/__init__.py
@@ -9,7 +9,6 @@ from esphome import pins
 CODEOWNERS = ["@edwardtfn"]
 
 nspanel_ha_blueprint_ns = cg.esphome_ns.namespace('nspanel_ha_blueprint')
-# MdiIcons = nspanel_ha_blueprint_ns.class_('MdiIcons', cg.Component)
 
 PSRAM_CLK_PIN = "psram_clk_pin"
 PSRAM_CS_PIN  = "psram_cs_pin"
@@ -44,9 +43,6 @@ async def to_code(config):
         add_idf_sdkconfig_option("CONFIG_BT_ALLOCATION_FROM_SPIRAM_FIRST", True)
         add_idf_sdkconfig_option("CONFIG_BT_BLE_DYNAMIC_ENV_MEMORY", True)
         add_idf_sdkconfig_option("CONFIG_ESP32_REV_MIN_3", True)
-        add_idf_sdkconfig_option("CONFIG_ESPTOOLPY_FLASHSIZE", "4MB")         # https://github.com/esphome/issues/issues/5404
-        add_idf_sdkconfig_option("CONFIG_ESPTOOLPY_FLASHSIZE_4MB", True)      # https://github.com/esphome/issues/issues/5404
-        # add_idf_sdkconfig_option("CONFIG_LWIP_MAX_SOCKETS", 5)                # This breaks web_server
         add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_BUFFER", True)
         add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_FREE_CA_CERT", True)
         add_idf_sdkconfig_option("CONFIG_MBEDTLS_DYNAMIC_FREE_CONFIG_DATA", True)
@@ -57,7 +53,3 @@ async def to_code(config):
 
     cg.add_define("USE_NSPANEL_HA_BLUEPRINT")
     cg.add_global(nspanel_ha_blueprint_ns.using)
-
-    # Setup the MdiIcons class instance
-    # mdi_icons = cg.new_Pvariable('mdi_icons')
-    # yield cg.register_component(mdi_icons, config)
