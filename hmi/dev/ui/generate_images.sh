c8c12a00b69c03a3f79650029894b665cb3a753a
Zsolt Sz Sztupák <mail@sztupy.hu>
2025-06-06 12:08:25 +0100

    Add support for easily changing backgrounds
    Changing the background of the panel was a common request, but due to technical limitations in the Nextion panel's handling of images it was not a straightforward task to do. This commit tries to fix this by introducing 3 changes to make the process more streamlined.
    
    1. Making sure cropped images are always on the same place
    
    This mainly involves the weather icons, as they are in a slightly different place between the Home page and the Weather pages. The changes make sure they are at the same place as the Home page everywhere. On the US version this also requires the temperature bit to move as well on the Weather page slightly.
    
    The other element that needs to be moved was the Fan control on the US version, so it is in the same place as the Cover control
    
    A couple other minor fixes in the HMI files include making sure texts properly have the "Crop Image" setting applied instead of a black background on a few pages, and conversely it should be "Solid Color" on the few pages where the background is fixed black. Although unused the home_smpl page was also fixed up in the US version as it had misaligned boxes.
    
    2. Autogenerate opaque images
    
    Since Nextion doesn't support transparent PNGs they need to be made opaque in pre-processing. To make this task easier there's a new command called `generate_images.sh` in the `ui` folder that will take the transparent images and put the background behind them at the appropriate place (for the weather icons and cover controls).
    
    Note: the transparent images have been generated from the black background ones using imagemagick's remove background feature for the weather icons, and manually makind the images transparent on some other pages (climate control, settings). If the original layered images are available (e.g. in XCF or PSD) I could generate these files more properly
    
    3. Automate TFT generation
    
    The third hurdle is usually the fact that Nextion Editor is a Windows GUI tool without any command line interface to make changes in the HMI files (and TFT compilation) easy. I am trying to address this with a separate project at https://github.com/sztupy/nextion-editor-docker which is a docker container containing Wine, Nextion Editor and AutoHotKey allowing some changes to be done in an automated way. There are currently two scripts that are likely useful:
    
    * Update the version number in the HMI file and generate a TFT
    * Replace all images in the HMI file and generate a TFT
    
    The second bit I also made it into a Home Assistant addon, where you can put your background files in the `/config` directory, and run the scripts to generate tft files at the `http://homeassistant.local:8123/local/<name>.tft` location, allowing easy updates afterwards from the ESPHome device page
    
    Example images at https://github.com/sztupy/nextion-editor-docker

---
+++ hmi/dev/ui/generate_images.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+#
+# Usage: ./generate_images <background_file_to_use> <eu*|us*>
+#
+# Default values are: ./generate_images pics/0.png eu
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+BACKGROUND=${1-"pics/0.png"}
+VARIANT=${2-"eu"}
+
+magick_command() {
+  if command -v magick >/dev/null 2>&1; then
+    pushd $SCRIPT_DIR
+      magick "$@"
+    popd
+  elif command -v convert >/dev/null 2>&1; then
+    pushd $SCRIPT_DIR
+      convert "$@"
+    popd
+  else
+    docker run --rm -v ${SCRIPT_DIR}:/imgs dpokidov/imagemagick "$@"
+  fi
+}
+
+# Cleanup
+rm -f $SCRIPT_DIR/$VARIANT/*.png $SCRIPT_DIR/$VARIANT/*.jpg || true
+mkdir -p $SCRIPT_DIR/$VARIANT
+
+if [[ $VARIANT = eu* ]]; then
+  echo "Generating Landscape images"
+else
+  echo "Generating Portrait images"
+fi
+
+# 0: Background
+cp $BACKGROUND $SCRIPT_DIR/$VARIANT/0.png
+
+# 1-15 Weather icons
+for i in {1..15}; do
+  if [[ $VARIANT = eu* ]]; then
+    magick_command $VARIANT/0.png pics/$i.png -geometry +20+45 -composite -background black -flatten -crop 100x100+20+45 +repage $VARIANT/$i.png
+  else
+    magick_command $VARIANT/0.png pics/$i.png -geometry +20+154 -composite -background black -flatten -crop 100x100+20+154 +repage $VARIANT/$i.png
+  fi
+done
+
+# 19,20 Cover controls
+# 39,40 Fan settings
+
+for i in 19 20 39 40; do
+  if [[ $VARIANT = eu* ]]; then
+    magick_command $VARIANT/0.png pics/$i.png -geometry +183+67 -composite -background black -flatten -crop 83x233+183+67 +repage $VARIANT/$i.png
+  else
+    magick_command $VARIANT/0.png pics/$i.png -geometry +119+143 -composite -background black -flatten -crop 83x233+119+143 +repage $VARIANT/$i.png
+  fi
+done
+
+# 33,34 Settings page
+# 37 Climate controls
+# 46,47 Home Alt
+for i in 33 34 37 46 47; do
+  if [[ $VARIANT = eu* ]]; then
+    magick_command $VARIANT/0.png pics/$i.png -geometry +0+0 -composite -background black -flatten -crop 480x320 +repage $VARIANT/$i.png
+  else
+    magick_command $VARIANT/0.png pics/$i-portrait.png -geometry +0+0 -composite -background black -flatten -crop 320x480 +repage $VARIANT/$i.png
+  fi
+done
+
+# Keep these with black background, either as they are on a black screen or they don't have transparency (e.g. sliders)
+
+# 16: Slider on Settings page
+# 17: Colour page
+# 18: Slider on Cover, Fan & Colour pages
+# 21-29: Colour page
+# 30,31: Indicators
+# 38: Slider on Climate page
+# 44,45: Alarm
+
+# 35,36,41,42,43: Unused?
+for i in {16..18} {21..31} 35 36 38 {41..45}; do
+  cp ${SCRIPT_DIR}/pics/$i.png ${SCRIPT_DIR}/$VARIANT/$i.png
+done
+
+# 32: Splash screen (JPG!)
+if [[ $VARIANT = eu* ]]; then
+  cp ${SCRIPT_DIR}/pics/32.jpg ${SCRIPT_DIR}/$VARIANT/32.jpg
+else
+  cp ${SCRIPT_DIR}/pics/32-portrait.jpg ${SCRIPT_DIR}/$VARIANT/32.jpg
+fi
