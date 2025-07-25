0a206c13337422d9b3b16ef6383736e91ee295b6
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-07-25 11:10:34 +0200

    Replace `is_safe_mode` by `current_panel_state`
    It takes a few bytes more, but adds quite a lot of possibilities

--- components/nspanel_ha_blueprint/base.h
+++ components/nspanel_ha_blueprint/base.h
@@ -7,14 +7,43 @@
 #ifndef CORE_BASE_H
 #define CORE_BASE_H
 
+#include <cstdint>
+
 namespace nspanel_ha_blueprint {
 
-    static const char* TAG_COMPONENT = "nspanel_ha_blueprint";
+    static const char *TAG_COMPONENT = "nspanel_ha_blueprint";
 
-    extern bool is_safe_mode;
+    enum class PanelState : uint8_t {
+        UNKNOWN = 0,
+        BOOTING = 1,
+        RUNNING = 2,
+        RUNNING_DIMMED = 3,
+        SLEEPING = 4,
+        UPDATING_TFT = 5,
+        RESTARTING = 6,
+        SAFE_MODE = 7
+    };
+
+    // Inline function to convert enum to string
+    inline const char *panel_state_to_string(PanelState state) {
+        switch (state) {
+            case PanelState::UNKNOWN: return "Unknown";
+            case PanelState::BOOTING: return "Booting";
+            case PanelState::RUNNING: return "Running";
+            case PanelState::RUNNING_DIMMED: return "Running (Dimmed)";
+            case PanelState::SLEEPING: return "Sleeping";
+            case PanelState::UPDATING_TFT: return "Updating TFT";
+            case PanelState::RESTARTING: return "Restarting";
+            case PanelState::SAFE_MODE: return "Safe mode";
+            default: return "Unknown";
+        }
+    }
+
+    // Global panel state variable declaration
+    extern PanelState current_panel_state;
 
 }  // namespace nspanel_ha_blueprint
 
-#endif // CORE_BASE_H
+#endif  // CORE_BASE_H
 
-#endif // NSPANEL_HA_BLUEPRINT_CORE_BASE
+#endif  // NSPANEL_HA_BLUEPRINT_CORE_BASE
