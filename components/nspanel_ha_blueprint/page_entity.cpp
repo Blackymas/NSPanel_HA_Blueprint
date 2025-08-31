e5d4b86cbbd4613b2f256b7cd66378a447b51206
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-08-31 16:58:34 +0200

    Move `set_bool` action to script `set_bool_action`

---
+++ components/nspanel_ha_blueprint/page_entity.cpp
@@ -0,0 +1,17 @@
+// page_entity.cpp
+
+#ifdef NSPANEL_HA_BLUEPRINT_PAGE_ENTITY
+
+#include "page_entity.h"
+
+namespace nspanel_ha_blueprint {
+
+    // Global var for remember if each page entity have content
+    bool is_entitypage01 = false;
+    bool is_entitypage02 = false;
+    bool is_entitypage03 = false;
+    bool is_entitypage04 = false;
+
+}  // namespace nspanel_ha_blueprint
+
+#endif  //NSPANEL_HA_BLUEPRINT_PAGE_ENTITY
