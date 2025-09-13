a0835c617f5fe9fb47bff02c60859009001db08f
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-09-13 21:02:25 +0200

    Move `set_climate` params to component
    It was crashing as parameters

--- components/nspanel_ha_blueprint/page_climate.cpp
+++ components/nspanel_ha_blueprint/page_climate.cpp
@@ -6,6 +6,17 @@
 
 namespace nspanel_ha_blueprint {
 
+    float set_climate_current_temp;
+    uint32_t set_climate_supported_features;
+    float set_climate_target_temp;
+    float set_climate_target_temp_high;
+    float set_climate_target_temp_low;
+    uint8_t set_climate_temp_step;
+    uint8_t set_climate_total_steps;
+    uint8_t set_climate_temp_offset;
+    std::string set_climate_climate_icon;
+    bool set_climate_embedded_climate;
+
 }  // namespace nspanel_ha_blueprint
 
 #endif  // NSPANEL_HA_BLUEPRINT_PAGE_CLIMATE
--- components/nspanel_ha_blueprint/page_climate.h
+++ components/nspanel_ha_blueprint/page_climate.h
@@ -4,7 +4,21 @@
 
 #ifdef NSPANEL_HA_BLUEPRINT_PAGE_CLIMATE
 
+#include <cstdint>
+#include <string>
+
 namespace nspanel_ha_blueprint {
+
+    extern float set_climate_current_temp;
+    extern uint32_t set_climate_supported_features;
+    extern float set_climate_target_temp;
+    extern float set_climate_target_temp_high;
+    extern float set_climate_target_temp_low;
+    extern uint8_t set_climate_temp_step;
+    extern uint8_t set_climate_total_steps;
+    extern uint8_t set_climate_temp_offset;
+    extern std::string set_climate_climate_icon;
+    extern bool set_climate_embedded_climate;
 
 }  // namespace nspanel_ha_blueprint
 
