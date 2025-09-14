f06d2fe7f791fa34465be9df47eb61988b15da8a
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-09-14 20:41:00 +0200

    Remove text sensors `notification_label` and `notification_text`
    To save memory.

---
+++ components/nspanel_ha_blueprint/page_notification.cpp
@@ -0,0 +1,14 @@
+// page_notification.cpp
+
+#ifdef NSPANEL_HA_BLUEPRINT_PAGE_NOTIFICATION
+
+#include "page_notification.h"
+
+namespace nspanel_ha_blueprint {
+
+  std::string notification_label;
+  std::string notification_text;
+
+}  // namespace nspanel_ha_blueprint
+
+#endif  // NSPANEL_HA_BLUEPRINT_PAGE_NOTIFICATION
---
+++ components/nspanel_ha_blueprint/page_notification.h
@@ -0,0 +1,16 @@
+// page_notification.h
+
+#pragma once
+
+#ifdef NSPANEL_HA_BLUEPRINT_PAGE_NOTIFICATION
+
+#include <string>
+
+namespace nspanel_ha_blueprint {
+
+  extern std::string notification_label;
+  extern std::string notification_text;
+
+}  // namespace nspanel_ha_blueprint
+
+#endif  // NSPANEL_HA_BLUEPRINT_PAGE_NOTIFICATION
