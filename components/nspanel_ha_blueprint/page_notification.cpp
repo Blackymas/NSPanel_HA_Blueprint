b471789d941a216aca0344ac02567855d45cd688
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-09-15 01:09:40 +0200

    Implement `ComponentVisibility` engine and transfer `home.bt_notific` updates to ESPHome

--- components/nspanel_ha_blueprint/page_notification.cpp
+++ components/nspanel_ha_blueprint/page_notification.cpp
@@ -8,6 +8,8 @@ namespace nspanel_ha_blueprint {
 
   std::string notification_label;
   std::string notification_text;
+  uint16_t notification_icon_color_normal = 52857;
+  uint16_t notification_icon_color_unread = 63488;
 
 }  // namespace nspanel_ha_blueprint
 
--- components/nspanel_ha_blueprint/page_notification.h
+++ components/nspanel_ha_blueprint/page_notification.h
@@ -4,12 +4,15 @@
 
 #ifdef NSPANEL_HA_BLUEPRINT_PAGE_NOTIFICATION
 
+#include <cstdint>
 #include <string>
 
 namespace nspanel_ha_blueprint {
 
   extern std::string notification_label;
   extern std::string notification_text;
+  extern uint16_t notification_icon_color_normal;
+  extern uint16_t notification_icon_color_unread;
 
 }  // namespace nspanel_ha_blueprint
 
