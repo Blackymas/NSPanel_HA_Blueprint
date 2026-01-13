bd47ddf900c50671d79651d7ba525650fd25844f
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-01-13 08:38:27 +0000

    Move the `StringRef` back to the component, with `inline`

--- components/nspanel_ha_blueprint/pages.cpp
+++ components/nspanel_ha_blueprint/pages.cpp
@@ -9,13 +9,4 @@ namespace nspanel_ha_blueprint {
     uint8_t last_page_id = UINT8_MAX;
     uint8_t wakeup_page_id = 1;
 
-    uint8_t get_page_id(const char* page_name) {
-        for (uint8_t i = 0; i < PAGE_COUNT; ++i) {
-            if (strcmp(page_names[i], page_name) == 0) {
-                return i;
-            }
-        }
-        return UINT8_MAX;
-    }
-
 }  // namespace nspanel_ha_blueprint
